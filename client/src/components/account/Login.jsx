5be88e40c7154a74f5307442fa390761f63e775a
U Harshitha <harshitha.srikk@gmail.com>
2024-05-06 10:42:38 +0530

    Initial Files

---
+++ client/src/components/account/Login.jsx
@@ -0,0 +1,14 @@
+import {Box, TextField} from '@mui/material'
+
+const Login = () => {
+    return(
+        <Box>
+            <img src = {'https://www.google.com/imgres?q=aws&imgurl=https%3A%2F%2Fwww.padok.fr%2Fhubfs%2FWebsite%25202021%2FIllustrations%2FAws-padok.png&imgrefurl=https%3A%2F%2Fwww.padok.fr%2Fen%2Famazon-web-services-aws&docid=YDKZOIuTEP9jZM&tbnid=34CT_pzFh_1WRM&vet=12ahUKEwiOwIe64e6FAxW1d2wGHQNzD8QQM3oECGgQAA..i&w=878&h=879&hcb=2&ved=2ahUKEwiOwIe64e6FAxW1d2wGHQNzD8QQM3oECGgQAA'} alt = "login"/>
+            <TextField>
+                
+            </TextField>
+        </Box>
+    )
+}
+
+export default Login;
